03da09a89e70ac4c231b199956476dd5b8db3b98
Edu Lazaro <eduzro@gmail.com>
2022-11-17 03:21:34 +0100

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,7 +89,7 @@ const config = {
               },
               {
                 label: 'API',
-                to: '/docs/api',
+                to: '/docs/category/api',
               },
             ],
           },
